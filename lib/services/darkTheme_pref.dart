bc6674ffefc07a842f9da9d39ceede98c6e7852b
CodeWithHasnain <hasnainhaiderbs2099@gmail.com>
2023-09-06 00:11:59 +0500

    dark_theme_pref

---
+++ lib/services/darkTheme_pref.dart
@@ -0,0 +1,14 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class DarkThemePref{
+   static const THEME_PREF  = 'THEMEPREF';
+  setDarkTheme(bool  value)async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+     pref.setBool(THEME_PREF, value);
+  }
+  Future<bool> getTheme()async{
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    return pref.getBool(THEME_PREF) ?? false;
+  }
+   
+}
