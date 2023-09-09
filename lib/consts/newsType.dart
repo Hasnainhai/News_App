f2586c94f8feda0750b7d0d8ea39da84a5784513
CodeWithHasnain <hasnainhaiderbs2099@gmail.com>
2023-09-09 16:49:56 +0500

    working on tabs

---
+++ lib/consts/newsType.dart
@@ -0,0 +1,5 @@
+
+enum NewsType{
+  allNews,
+  topTrandings,
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:grocery_app/Widgets/drawer_widget.dart';
-import 'package:grocery_app/provider/dark_theme_provide.dart';
+import 'package:grocery_app/Widgets/tabs_widget.dart';
+import 'package:grocery_app/consts/newsType.dart';
 import 'package:grocery_app/services/utils.dart';
-import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,10 +12,11 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 class _HomeScreenState extends State<HomeScreen> {
+   var newsType =  NewsType.allNews ;
   @override
   Widget build(BuildContext context) {
-      final themeState = Provider.of<DarkThemeProvide>(context);
       final Color color = Utils(context).getColor;
+     
     return Scaffold(
       drawer: const DrawerWidget(),
       appBar: AppBar( 
@@ -24,7 +25,6 @@ class _HomeScreenState extends State<HomeScreen> {
                   style: GoogleFonts.lobster(
                     textStyle: TextStyle(color: color,fontSize: 20.0,letterSpacing: 0.6)
                   ),
-                  
                 ),
                 centerTitle: true,
       iconTheme: IconThemeData(color: color),
@@ -34,7 +34,41 @@ class _HomeScreenState extends State<HomeScreen> {
       IconButton(onPressed: (){}, icon:const Icon(Icons.search))
     ],
       ),
-      body:Container(),
+      body: Column(
+        children: [
+          Padding(padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              TabsWidget(text: 'All News',
+               color: newsType == NewsType.allNews ? Theme.of(context).cardColor : Colors.transparent, 
+               ontap: (){
+                if(newsType == NewsType.allNews){
+                   return;
+                }
+               
+                setState(() {
+                  newsType = NewsType.allNews;
+                });
+               },
+              fontSize: newsType == NewsType.allNews ? 22 : 14,),
+             const  SizedBox(width: 20.0),
+               TabsWidget(text: 'top Trandings',
+               color: newsType == NewsType.topTrandings ? Theme.of(context).cardColor : Colors.transparent,
+               ontap: (){
+                if(newsType == NewsType.topTrandings){
+                             return;
+                }
+                  setState(() {
+                    newsType = NewsType.topTrandings;
+                  });
+                
+               },
+              fontSize: newsType == NewsType.topTrandings ? 22 : 14,),
+            ],
+          ),)
+        ],
+      ),
     );
   }
 }
+
